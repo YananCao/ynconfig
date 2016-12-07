58c645c6d116065b02ac21e5412eb2d35ea181bb
Jackie <cyn_rich@163.com>
2016-12-07 14:24:58 +0800

    update util description, remove invalid file

--- util/gen_nginx_agent_config/gen_nginx_proxy_conf.py
+++ util/gen_nginx_agent_config/gen_nginx_proxy_conf.py
@@ -3,16 +3,13 @@
 """
   This tool can generate nginx configuration files,
   while the domain name of the agent is added to
-  the '/etc/hosts' file mapped to '127.0.0.1'
+  the 'hosts' file mapped to '127.0.0.1'
   Create by Cao Ya'nan <cyn_rich@163.com> at 2016-12-06 12:17
 """
 import os
 
 
 def gen_nginx_conf():
-    """生成nginx反向代理配置文件，并将被代理域名添加到
-    '/etc/hosts'文件中，映射为'127.0.0.1'
-    """
     data_file_path = 'reverse_data.md'
     nginx_file_path = 'nginx_agent_conf'
     if not os.path.exists(data_file_path):
